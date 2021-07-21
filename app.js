38a3e6d644464002fb3a75be44a4728f685bda3a
Rohit Gadhave <RohitGadhave249@gmail.com>
2021-07-21 15:50:54 +0530

    done with testing

--- app.js
+++ app.js
@@ -10,35 +10,73 @@ const app = express();
 //set STORAGE engin
 
 const storage = multer.diskStorage({
-    destination:"./public/uploads/",
-    filename:(req , file,calback)=>{
-        calback(null,Date.now()+path.extname(file.originalname));
+    destination: "./public/uploads/",
+    filename: (req, file, calback) => {
+        calback(null, Date.now()+ '-' + Math.round(Math.random() * 1E9)+'-' + path.extname(file.originalname));
     }
 });
 
 //Init Upload
 
 const upload = multer({
-    storage:storage
+    storage: storage,
+    limits: { fileSize: 1000000 },//size in bytes
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
 }).single('img');
 
+// Check File Type
+function checkFileType(file, cb) {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
 //EJS
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
 //PUBLIC FOLDER
 
 app.use(express.static('./public'));
 
 //routs
-app.get('/' , (req , res)=>{
+app.get('/', (req, res) => {
 
-   res.render('index')
+    res.render('index')
 
-})
-app.post('/upload' , (req , res)=>{
+});
+app.post('/upload', (req, res) => {
 
-   res.send('hello from simple server :)')
 
-})
+    upload(req, res, (error) => {
+        //check for error
+        if (error) {
+            //res.json({error:error});
+            res.render('index', { msg: error });
+        } else {
+            if (req.file == undefined) {
+                res.render('index', {
+                    msg: 'Error: No File Selected!'
+                });
+            } else {
+                //sore req.file obj in db
+                res.render('index', {
+                    msg: 'File Uploaded!',
+                    file: `uploads/${req.file.filename}`,
+                    fileinfo: req.file
+                });
+            }
+        }
+    });
+});
 
-app.listen(3000,()=>console.log("server running"));
+app.listen(3000, () => console.log("server running"));
